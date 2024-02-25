d3afddfd047b7fa3598dccbbe739990a2574e9b1
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2024-02-25 10:13:29 +0100

    Modified the build.sh script to build the react-app folder containing the application developed by Collabrium for Natixar.

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@
 (cd web-src; hugo --gc --minify)
 
 # Build the anima hello world web app in React
-(cd hello-world && yarn install && yarn build)
+#(cd hello-world && yarn install && yarn build)
 #(cd hello-world; npm run build)
 
 # Build Collabrium's front end
-# ...
+(cd react-app && yarn install && yarn build)
+
 
