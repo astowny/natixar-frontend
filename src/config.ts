89ec0bb2362529cd2b15f92f9629711da3a81e96
Alexander Shelyugov <ashelyugov@protonmail.com>
2024-04-03 18:45:55 +0200

    Finish Mantis removal

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,7 @@ import {
 
 // ==============================|| THEME CONSTANT ||============================== //
 
-export const twitterColor = "#1DA1F2"
-export const facebookColor = "#3b5998"
-export const linkedInColor = "#0e76a8"
-
-export const APP_DEFAULT_PATH = "/dashboard/analytics"
+export const APP_DEFAULT_PATH = "/"
 export const HORIZONTAL_MAX_ITEM = 7
 export const DRAWER_WIDTH = 260
 export const MINI_DRAWER_WIDTH = 60
@@ -29,12 +25,6 @@ const configNatixar: DefaultConfigProps = {
   presetColor: "default",
   themeDirection: ThemeDirection.LTR,
   isShowExtraHeader: false,
-}
-
-export const fusionConfig = {
-  clientID: "5e9cba0b-4978-4a24-88c0-0a45b0ed067f",
-  serverUrl: "http://localhost:8011",
-  redirectUri: "http://localhost:3000",
 }
 
 export default configNatixar
